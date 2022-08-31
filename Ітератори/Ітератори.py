d60c5d9f62622bf5bc800dbf6e99cbc766c02fa1
Cruserschaft <cruserschaft@gmail.com>
2022-08-31 17:48:00 +0300

    corrections

--- Ітератори/Ітератори.py
+++ Ітератори/Ітератори.py
@@ -1,17 +1,16 @@
 #1 Завдання
-##1) Реализуйте генераторную функцию, которая будет возвращать по
-##одному члену геометрической прогрессии с указанным множителем.
-##Генератор должен остановить свою работу или по достижению указанной
-##границы, или при передаче команды на завершение.
+#1) Реализуйте генераторную функцию, которая будет возвращать по
+#одному члену геометрической прогрессии с указанным множителем.
+#Генератор должен остановить свою работу или по достижению указанной
+#границы, или при передаче команды на завершение.
 
 
-stop = 100
-def geom_progression(value):
-    if isinstance(value, int|float):
-        return (i * value for i in range(1, stop))
-
-
-
+def geom_progression(value, start = 1, end = 100):    
+    while start < end:
+        yield start
+        start*=value
+        
+        
 x = geom_progression(3)
 print(x)
 print(next(x))
@@ -19,28 +18,55 @@ print(next(x))
 print(next(x))
 print(next(x))
 
-y = geom_progression(17.5)
+y = geom_progression(5)
 print(x)
 print(next(y))
 print(next(y))
 print(next(y))
-print(next(y))
+#print(next(y)) #Stop iteration
+
+
 
 #2 Завдання
 ##2) Реализуйте свой аналог генераторной функции range(). Да, вы теперь
 ##знаете, что это - генератор.
 
 
-def my_range(start, stop):
-    if not isinstance(start, int) and not isinstance(stop, int):
-        raise ValueError("Must be integer")
-    return (i for i in range(start, stop))
+def my_range(*args):
+    if len(args) == 1:
+        start, stop, step = 0, args[0], 1
 
-x = my_range(10, 20)
+    if len(args) == 2:
+        start, stop, step = args[0], args[1], 1
+
+    if len(args) == 3:
+        start, stop, step = args[0], args[1], args[2]
+
+    if step >= 0 and start > stop:
+        raise ValueError()
+
+
+    while start < stop:
+        yield start
+        start += step
+
+
+x = my_range(10)
 print(x)
 print(next(x))
-print(next(x))
-print(next(x))
+
+y = my_range(2, 10)
+print(y)
+print(next(y))
+print(next(y))
+print(next(y))
+
+z = my_range(2, 10, 2)
+print(z)
+print(next(z))
+print(next(z))
+print(next(z))
+print(next(z))
 
 
 #3 Завдання
--- Замикання/2 Фибоначі.py
+++ Замикання/2 Фибоначі.py
@@ -1,3 +1,5 @@
+import timeit
+
 """2) Используя функцию замыкания реализуйте такой прием программирования
 как Мемоизация - https://en.wikipedia.org/wiki/Memoization
 Используйте полученный механизм для ускорения функции рекурсивного
@@ -25,6 +27,82 @@ print(x(7))
 print(x(3))
 print(x(20))
 print(x(4))
+
+
+def recurse_fibonacci():
+    def get_fib(n):
+        results = [0, 1]
+        for i in range (n-len(results)):
+            results.append(results[-1]+results[-2])
+        return results[-1]
+
+    return get_fib
+
+
+y = recurse_fibonacci()
+
+print(y(5))
+print(y(7))
+print(y(3))
+print(y(20))
+print(y(4))
+
+
+
+
+
+
+first = """
+def fibonacci():
+    results = [0, 1, ]
+
+    def get_fib(n):
+        if n < len(results):
+            return results[n-1]
+        for i in range (n-len(results)):
+            results.append(results[-1]+results[-2])
+        return results[-1]
+
+    return get_fib
+
+
+x = fibonacci()
+
+x(5)
+x(7)
+x(3)
+x(20)
+x(4)
+"""
+
+second = """
+def recurse_fibonacci():
+    def get_fib(n):
+        results = [0, 1]
+        for i in range (n-len(results)):
+            results.append(results[-1]+results[-2])
+        return results[-1]
+
+    return get_fib
+
+
+y = recurse_fibonacci()
+
+y(5)
+y(7)
+y(3)
+y(20)
+y(4)
+"""
+
+
+
+print(timeit.timeit(first, number=1000000)) #4.6
+print(timeit.timeit(second, number=1000000)) #6.2
+
+
+
+
 
 
 
