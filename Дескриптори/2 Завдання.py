9b561d9ea16f7e6710d8c81ed7fd07b65c7e992e
Cruserschaft <cruserschaft@gmail.com>
2022-09-10 17:04:57 +0300

    corrections

--- Дескриптори/2 Завдання.py
+++ Дескриптори/2 Завдання.py
@@ -3,25 +3,40 @@
 #попытаться присвоить, например, строку, то должно быть возбужденно
 #исключение.
 
+class Descriptor:
+    def __set_name__(self, owner, name):
+        self.name = name
+
+    def __set__(self, instance, value):
+        if not isinstance(value, int):
+            raise TypeError
+        self.val = value
+
+    def __get__(self, instance, owner):
+        return self.val
+
+
 class Box:
+    x = Descriptor()
+    y = Descriptor()
+    z = Descriptor()
+
     def __init__(self, x, y, z):
         self.x = x
         self.y = y
         self.z = z
 
-
-    def __get__(self, instance, owner):
+    def volume(self):
         return self.x * self.y * self.z
-
-    def __setattr__(self, key, value):
-        if not isinstance(value, int):
-            raise TypeError("Coords must be integer")
-        self.__dict__[key] = value
 
     def __str__(self):
         return f"{self.x * self.y * self.z}"
 
-b = Box(1, 2, 3)
-print(b)
+
+
+
+b1 = Box(1, 2, 3)
+print(b1)
+
 
 #c = Box("hu", 3, 5)
